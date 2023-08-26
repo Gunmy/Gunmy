6a322f6b8610fde5eb251df7e67448461402bd5b
Gunmy <99408493+Gunmy@users.noreply.github.com>
2023-08-26 22:46:11 +0200

    Delete website directory

--- website/include/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "tilegame";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-?>
